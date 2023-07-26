dd8cda8048146c175c3f308848b6bc308bb9ad26
LLLike27 <lushuaizong@gmail.com>
2023-07-26 23:51:50 +0800

    初始化脚手架

---
+++ vite.config.js
@@ -0,0 +1,21 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  server: {
+    // 浏览器服务端端口
+    host: 'localhost',
+    port: 8000,
+    open: true
+  },
+  publicPath: '/'
+})
